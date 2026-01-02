c2f44b28cb20b342a85fbb503cedfe29893a281d
Varsha Thomas <varshathomas364@gmail.com>
2026-01-02 14:09:13 +0530

    Cancels all downloads, pause fails sometimes

--- chrome extension/background.js
+++ chrome extension/background.js
@@ -1,10 +1,26 @@
+function isUserInitiated(item)
+{
+    return (
+        (item.filename || item.url.startsWith("blob") || item.url.startsWith("http")) &&
+        item.totalBytes !== undefined        
+    );
+}
+
 function detectDownload(downloadItem) 
 {
     console.log("Download detected!");
     console.log("File: ", downloadItem.filename);
     console.log("URL: ", downloadItem.url);
-
-    chrome.downloads.pause(downloadItem.id);
+    console.log("Total Bytes:", downloadItem.totalBytes);
+    if (isUserInitiated(downloadItem))
+    {
+        chrome.downloads.cancel(downloadItem.id);
+        console.log("Download paused");
+    }
+    else
+    {
+        console.log("Ignored non-user initiated download", downloadItem.id);
+    }
 }
 
 chrome.downloads.onCreated.addListener(detectDownload);
